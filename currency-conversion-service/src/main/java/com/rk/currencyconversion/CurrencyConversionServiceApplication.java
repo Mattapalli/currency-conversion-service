cc2c6c772e45cbca0b2d801b7c3c36697e245cd9
RMattapalli <RMattapalli@HIBAML134346.ad.harman.com>
2020-01-28 23:27:22 +0530

    adding eureka-naming-server (to discover all the services)

--- currency-conversion-service/src/main/java/com/rk/currencyconversion/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/rk/currencyconversion/CurrencyConversionServiceApplication.java
@@ -2,11 +2,12 @@ package com.rk.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-
+@EnableEurekaServer
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
