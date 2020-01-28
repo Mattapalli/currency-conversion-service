fa61688f5b474ffa72cdb36e2deb1d64d0fee878
RMattapalli <RMattapalli@HIBAML134346.ad.harman.com>
2020-01-28 21:44:46 +0530

    adding feign client (interacting with other microservice(calling currency-exchange-service)

--- currency-conversion-service/src/main/java/com/rk/currencyconversion/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/rk/currencyconversion/CurrencyConversionServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- currency-conversion-service/src/main/java/com/rk/currencyconversion/feignclient/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/rk/currencyconversion/feignclient/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.rk.currencyconversion.feignclient;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,8 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.rk.currencyconversion.model.CurrencyConversionBean;
 
 
-
-@FeignClient(name="currency-exchange-service",url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service",url="localhost:8000")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
